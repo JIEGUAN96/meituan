6c6224fc32e66048fd5a20112f1524e1ea89c082
abc <abc@jieguan.local>
2016-03-12 00:19:33 +0800

    添加了person类

---
+++ 美团/美团/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  美团
+//
+//  Created by abc on 16/3/12.
+//  Copyright © 2016年 jieguan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ 美团/美团/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  美团
+//
+//  Created by abc on 16/3/12.
+//  Copyright © 2016年 jieguan. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
